958828012cbe7292918ec9a11e6ff3270bfb78b8
Daniel André da Silva <dap1995@gmail.com>
2018-09-29 17:12:22 -0300

    Show news details

--- src/scenes/Details.js
+++ src/scenes/Details.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import { PixelRatio, View, Text, ScrollView, Dimensions } from 'react-native';
+import HTML from 'react-native-render-html';
 
-const Details = () => (
-  <View>
-    <Text style={{ fontSize: 60 }}>
-      Detalhes
-    </Text>
-  </View>
-);
+const Details = ({ navigation }) => {
+  const { title, content } = navigation.state.params;
+  return (
+    <ScrollView contentContainerStyle={{ backgroundColor: 'white' }} style={{ flex: 1}}>
+      <View style={{ flex: 1, padding: 10, backgroundColor: '#AED5EC' }}>
+        <Text style={{ fontSize: 30 }}>
+          {title}
+        </Text>
+      </View>
+      <View style={{ flex: 5, padding: 20 }}>
+        <HTML
+          style={{ flex: 1 }}
+          html={content}
+          baseFontStyle={{ fontSize: 20 }}
+          imagesMaxWidth={Dimensions.get('window').width}
+          ptSize={PixelRatio.get()}
+        />
+      </View>
+    </ScrollView>
+  );
+}
 
 export default Details;
