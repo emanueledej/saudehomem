d3ebe8ee1e7e9d821fb95a04a269095ed6915aac
Daniel André da Silva <dap1995@gmail.com>
2018-09-29 18:15:54 -0300

    Show a message to user that recommended exams will be available soon

--- src/scenes/Exams.js
+++ src/scenes/Exams.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
 import Header from '../components/Header';
 
 const Details = ({ navigation }) => {
   return (
     <ScrollView style={{ backgroundColor: 'white' }}>
       <Header title={'Exames Recomendados'} />
+      <View
+        style={{
+          alignItems: 'center',
+          padding: 10,
+        }}
+      >
+        <Text
+          style={{
+            textAlign: 'center',
+            fontSize: 20,
+          }}
+        >
+          A consulta de exames recomendados estará disponível em breve!{'\n\n'}
+        </Text>
+      </View>
+      <View style={{ alignItems: 'center' }}>
+        <Text style={{ fontSize: 80, textAlign: 'center' }}>😄</Text>
+      </View>
     </ScrollView>
   );
 }
