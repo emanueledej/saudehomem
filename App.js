6bbf39a668042d48829b68a0848cafd8451dd73e
Lucas Kupas <luksfk@gmail.com>
2018-10-09 22:21:33 -0300

    push notifications added

--- App.js
+++ App.js
@@ -6,18 +6,17 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  Button,
-  SafeAreaView,
-} from 'react-native';
+import React, { Component } from 'react';
+import { SafeAreaView } from 'react-native';
 import Router from './src/modules/Router';
+import OneSignal from 'react-native-onesignal';
 
 export default class App extends Component {
+
+  componentWillMount() {
+    OneSignal.init("5e88f222-9236-4542-84d9-ad1b38297e83");
+  }
+
   render() {
     return (
       <SafeAreaView style={{ flex: 1 }}>
